887e51941853e17748f5d455c78ec22177e0f89d
Adriano Nascimento <Adriano Nascimento@WAR-MACHINE>
2017-05-23 20:55:37 -0300

    Alteração da tela

--- CordovaApp/CordovaApp/www/scripts/index.js
+++ CordovaApp/CordovaApp/www/scripts/index.js
@@ -1,18 +1,18 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
+﻿const PARAMETROS = {
+    QTD_REGISTROS : 100,    
+}
+
+var db = window.openDatabase(name, version, displayName, estimatedSize);
+
+
 (function () {
     "use strict";
-
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
-        // Handle the Cordova pause and resume events
+        
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -20,11 +20,24 @@
         receivedElement.setAttribute('style', 'display:block;');
     };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+    function onPause() {};
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    function onResume() { };
+
+    document.getElementById("btnMultMatriz").addEventListener("click", multMatriz);
+    document.getElementById("btnInsertWebService").addEventListener("click",webService);
+    document.getElementById("btnInsertDB").addEventListener("click", insertdbLocal);
+
+})();
+
+function insertdbLocal() {
+    console.log("!1");
+}
+
+function webService() {
+    console.log("!2");
+}
+
+function multMatriz() {
+    console.log("!3");
+}
