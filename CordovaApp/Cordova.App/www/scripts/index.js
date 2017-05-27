043645b6bfae6cf7aac27ac962b504080cda023d
Adriano Nascimento <Adriano Nascimento@WAR-MACHINE>
2017-05-27 01:24:18 -0300

    add sqlplugin

--- CordovaApp/Cordova.App/www/scripts/index.js
+++ CordovaApp/Cordova.App/www/scripts/index.js
@@ -1,30 +1,68 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
+﻿const PARAMETROS = {
+    QTD_REGISTROS : 100,    
+}
+
+var db;
+
+
 (function () {
     "use strict";
-
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
-        // Handle the Cordova pause and resume events
+        db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' });
+        
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //var parentElement = document.getElementById('deviceready');
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
     };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+    function onPause() {};
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    function onResume() { };
+
+    document.getElementById("btnMultMatriz").addEventListener("click", multMatriz);
+    document.getElementById("btnInsertWebService").addEventListener("click",webService);
+    document.getElementById("btnInsertDB").addEventListener("click", insertdbLocal);
+
+})();
+
+function insertdbLocal() {
+    console.log(db);
+    console.log("!1");
+}
+
+function webService() {
+    console.log("!2");
+}
+
+function multMatriz() {
+    console.log("!3");
+}
+
+var queryInsert = "INSERT INTO Usuario(UsuarioId, Nome, Idade, Foto, Salario, Ativo) VALUES (?,?,?,?,?,?);";
+
+function createDb(){
+	db.transaction(function(tx) {
+	    tx.executeSql('CREATE TABLE IF NOT EXISTS Usuario(UsuarioId INTEGER, Nome VARCHAR(100), Idade INTEGER, Foto BLOB,Salario DOUBLE, Ativo SMALLINT, PRIMARY KEY(UsuarioId))');
+	}, function(error) {
+	    console.log('Transaction ERROR: ' + error.message);
+	}, function() {
+	    console.log('Populated database OK');	
+	});	
+}
+
+function insert(){
+    db.transaction(function(tx) {
+        tx.executeSql(queryInsert, [id, nome, idade, foto, salario, ativo]);
+    }, function(error) {
+        console.log('Transaction ERROR: ' + error.message);
+    }, function() {
+        console.log('Populated database OK');	
+    });	
+}
