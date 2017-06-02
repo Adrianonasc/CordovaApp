c4865d1413cd3c1565f1a01f52586c5f6451db76
Adriano Nascimento <Adriano Nascimento@WAR-MACHINE>
2017-06-01 22:46:19 -0300

    -Implementação das inserção em base local depois de consumir webservice

--- CordovaApp/Cordova.App/www/scripts/index.js
+++ CordovaApp/Cordova.App/www/scripts/index.js
@@ -1,44 +1,54 @@
 ﻿var TEMPO_INICIO = 0;
 var TEMPO_FIM = 0;
 var db;
-var arrTempoExecucaocucao = new Array();
-
+var arrTempoExecucao = new Array();
 var arrQuantRegistro = [10, 25, 50, 75, 100, 250, 1000];
 var indexQuantidadeRegisto = 0;
-
-var queryInsert = "INSERT INTO Usuario(UsuarioId, Nome, Idade, Foto, Salario, Ativo) VALUES (?,?,?,?,?,?);";
-
 
 (function () {
 	"use strict";
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-
 	function onDeviceReady() {
-		db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' });
-
 		document.addEventListener('pause', onPause.bind(this), false);
 		document.addEventListener('resume', onResume.bind(this), false);
+		db = window.sqlitePlugin.openDatabase({ name: 'my.db', location: 'default' });
 	};
-
 	function onPause() { };
-
 	function onResume() { };
-
 	document.getElementById("btnMultMatriz").addEventListener("click", multMatriz);
 	document.getElementById("btnInsertWebService").addEventListener("click", webService);
 	document.getElementById("btnInsertDB").addEventListener("click", insertdbLocal);
-
 })();
 
 function insertdbLocal() {
-	console.log(db);
-	console.log("!1");
+	if (indexQuantidadeRegisto >= arrQuantRegistro.length) indexQuantidadeRegisto = 0;
+
+	dropTable();
+	createTable();
+	var i = indexQuantidadeRegisto;
+	TEMPO_INICIO = performance.now();
+	var usuario;
+	var usuarios = [];
+	for (var j = 1; j <= arrQuantRegistro[i]; j++) {
+		usuario = { Id:0, Nome:'', Idade:0, Foto:null, salario:0 };
+		usuario.Id = j;
+		usuario.Nome = "Nome " + j;
+		usuario.Idade = j;
+		usuario.Foto = new ArrayBuffer(16);
+		usuario.salario = j * 10;
+		usuarios.push(usuario);
+	}
+	insert(usuarios);
+	indexQuantidadeRegisto++;
 }
 
 function webService() {
-	var URI = "http://webapiadrtcc.azurewebsites.net/api/Usuarios/";
+	if (indexQuantidadeRegisto >= arrQuantRegistro.length) indexQuantidadeRegisto = 0;
 
+	var URI = "http://webapiadrtcc.azurewebsites.net/api/Usuarios/";
 	console.log(URI + (arrQuantRegistro[indexQuantidadeRegisto] - 1));
+	dropTable();
+	createTable();
 
 	TEMPO_INICIO = performance.now();
 	$.ajax({
@@ -46,36 +56,63 @@ function webService() {
 		type: 'Get',
 		contentType: 'application/json',
 	}).done(function (data) {
-		TEMPO_FIM = performance.now();
+		insert(data);
 		document.getElementById("lblText").innerHTML = "Dados Recebido";
-
-		arrTempoExecucao.push(TEMPO_FIM - TEMPO_INICIO);
-		console.log("Tempo Execução :", (TEMPO_FIM - TEMPO_INICIO), " milliseconds");
 		indexQuantidadeRegisto += 1;
 	}).fail(function (jqXHR, textStatus, msg) {
 		document.getElementById("lblText").innerHTML = textStatus + msg;
 	});
 }
 
-
-function createDb() {
+function dropTable() {
 	db.transaction(function (tx) {
-		tx.executeSql('CREATE TABLE IF NOT EXISTS Usuarios(UsuarioId INTEGER, Nome VARCHAR(100), Idade INTEGER, Foto BLOB,Salario DOUBLE, Ativo SMALLINT, PRIMARY KEY(UsuarioId))');
+		tx.executeSql('DROP TABLE IF EXISTS Usuarios');
 	}, function (error) {
 		console.log('Transaction ERROR: ' + error.message);
 	}, function () {
+		console.log('Transaction Tabela Excluida');
+	});
+}
+
+function createTable() {
+	db.transaction(function (tx) {
+		tx.executeSql('CREATE TABLE IF NOT EXISTS Usuarios(UsuarioId INTEGER, Nome VARCHAR(100), Idade INTEGER, Foto BLOB,Salario DOUBLE, PRIMARY KEY(UsuarioId))');
+	}, function (error) {
+		console.log('Transaction ERROR: ' + error.message);
+	}, function () {
+		console.log('Transaction Tabela Criada');
+	});
+}
+
+function select() {
+	db.transaction(function (tx) {
+		tx.executeSql('SELECT count(*) AS qtd FROM Usuarios', [], function (tx, rs) {
+			console.log('Quantidade de Registro: ' + rs.rows.item(0).qtd);
+		}, function (tx, error) {
+			console.log('SELECT error: ' + error.message);
+		});
+	});
+}
+
+function insert(Usuarios) {
+	var queryInsert = "INSERT INTO Usuarios(UsuarioId, Nome, Idade, Foto, Salario) VALUES (?,?,?,?,?);";
+	db.transaction(function (tx) {
+		$(Usuarios).each(function (i) {
+			tx.executeSql(queryInsert, [Usuarios[i].Id, Usuarios[i].Nome, Usuarios[i].Idade, Usuarios[i].Foto, Usuarios[i].salario]);
+		});
+	}, function (error) {
+		console.log('Transaction ERROR: ' + error.message);
+	}, function () {
+		//grava o Tempo no Commit
+		TEMPO_FIM = performance.now();
+		arrTempoExecucao.push(TEMPO_FIM - TEMPO_INICIO);
+		exibeTempo();
 		console.log('Populated database OK');
 	});
 }
 
-function insert() {
-	db.transaction(function (tx) {
-		tx.executeSql(queryInsert, [id, nome, idade, foto, salario, ativo]);
-	}, function (error) {
-		console.log('Transaction ERROR: ' + error.message);
-	}, function () {
-		console.log('Populated database OK');
-	});
+function exibeTempo() {
+	console.log("Tempo Execução :", (TEMPO_FIM - TEMPO_INICIO), " milliseconds");
 }
 
 /*----Teste de Matriz -----*/
